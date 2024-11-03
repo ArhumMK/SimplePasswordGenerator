cd73abbe4413d79264040017abe0491b291fb943
ArhumMK <pattyjackson363@gmail.com>
2024-11-03 15:34:40 +0000

    basic password generation(i think this works)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+use rand::{thread_rng, Rng};
+
+const LOWERCASE: &str = "abcdefghijklmnopqrstuvwxyz";
+const UPPERCASE: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const NUMBERS: &str = "0123456789";
+const SYMBOLS: &str = "!@#$%^&*()_-+=<>?";
+
+fn generate_password() -> String {
+    let mut chars = LOWERCASE.to_string() + UPPERCASE + NUMBERS + SYMBOLS;
+    let mut rng = thread_rng();
+
+    (0..16)
+        .map(|_| {
+            let idx = rng.gen_range(0..chars.len());
+            chars.chars().nth(idx).unwrap()
+        })
+        .collect()
+}
+
 fn main() {
-    println!("Hello World!");
+    let password = generate_password();
+    println!("Generated password: {}", password);
 }
